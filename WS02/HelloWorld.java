360ca5bf4c264677ff41193e4b802a28dd893283
Mishell Estefania Moromenacho Tipan <75351548+MishellMoromenacho@users.noreply.github.com>
2021-01-03 23:26:31 -0500

    Add files via upload

---
+++ WS02/HelloWorld.java
@@ -0,0 +1,13 @@
+//Mishell Moromenacho
+/*
+this is a comment   
+in several linea
+and then ends
+in the final line
+*/
+
+class HelloWorld{    
+    public static void main(String[] args) {
+        System.out.println("Hello World from Mishell Moromenacho");
+    }
+}
